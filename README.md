3a7c923e8ee437cf3d8be44b1afe47d799568602
emmanuel <emmanuel@fulodev.com>
2023-11-23 13:13:59 +0000

    initial sudoku commit

--- README.md
+++ README.md
@@ -1,12 +1,3 @@
-## Tower of Hanoi Puzzle
+## Hanoi Sudoku Solver
 
-The *Tower of Hanoi*, also known as the *Benares Temple problem* or *Lucas' Tower*, is a classic mathematical game comprising three rods and various-sized disks. The disks start on one rod, forming a stack, and the goal is to move the entire stack to another rod while following specific rules:
-
-- Only one disk can be moved at a time.
-- Each move involves taking the uppermost disk from a stack and placing it on top of another stack or an empty rod.
-- A disk cannot be placed on top of a smaller disk.
-
-The puzzle's objective is to complete the task in the minimum number of moves. For a *Tower of Hanoi* puzzle with three disks, it can be solved in seven moves, while the general formula for the minimum moves needed is `2^n - 1`, where `n` is the number of disks.
-
-
-I am using this challenge to familiarise myself with recursion.
+Using recursion and backtracking to check if a sudoku puzzle is unsolvable.
--- main.py
+++ main.py
@@ -1,6 +1,41 @@
-def main():
-    hanoi()
+def find_empty(puzzle):
+    """
+    finds the next row, col on the puzzle that is not filled
+    empty spots are represented as -1
+
+    (row, column) tuple is returned or None
+    """
+
+    # 0-8 will be used for the index
+    for row in range(9):
+        for col in range(9):
+            if puzzle[row][col] == -1:
+                return row, col
+    
+    return None, None # no spaces left e.g no -1
 
 
-def hanoi():
-    pass
+def is_valid(puzzle, guess, row, col):
+    #check whether guess at paticular row/column is valid
+    #return True if valid, return False if not valid
+    
+    #start with row, easiest since every list in puzzle represents a row
+    
+
+def validate_puzzle(puzzle):
+    """
+    the puzzle is a list of lists
+    each inner list is a row in the sudoku puzzle
+    return whether or not a solution exists
+      -  if solution exits mutate puzzle to be the solution
+    """
+    row, col = find_empty(puzzle)
+
+    # only valid inputs allowed so if no spaces left problem is solved
+    if row is None:
+        return True
+
+    #if there is a space, make a guess between 1 and 9
+    for guess in range(1, 10):
+        #check if guess is valid
+        if is_valid(puzzle, guess, row, col):
